b49163d847ef7054a25aa8b13ac343f4013030c2
Rafael Matias <rafamatias102@gmail.com>
2024-06-04 20:56:39 +0000

    Django Practice

--- DataBox/wsgi.py
+++ DataBox/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for DriveClone project.
+WSGI config for DataBox project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "DriveClone.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "DataBox.settings")
 
 application = get_wsgi_application()
