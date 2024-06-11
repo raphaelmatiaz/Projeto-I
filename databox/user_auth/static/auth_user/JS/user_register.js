39e1131c117f6637eb57875290626ded5a3a245e
Rafael Matias <rafamatias102@gmail.com>
2024-06-11 14:59:16 +0000

    Finished basic structure & static functionality of register user/staff page

---
+++ databox/user_auth/static/auth_user/JS/user_register.js
@@ -0,0 +1,16 @@
+window.onload = () => {
+    
+    console.log("this is JS for user register page")
+    
+    // const staffUrl = "{% url 'register__staff' %}";
+
+    // const StaffButton = document.querySelector("#staff-button")
+    // StaffButton.addEventListener("click", () => {
+    //     console.log("button clicked")
+    //     window.location.href= staffUrl;
+    // })
+        
+    
+
+
+}
--- databox/user_auth/urls.py
+++ databox/user_auth/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.show_landing_page),
+    path('', views.show_landing_page, name='landing__page'),
     path('register-user/', views.show_register_user, name='register__user'),
     path('register-staff/', views.show_register_staff, name='register__staff'),
     path('login-user/', views.show_login_user, name='login__user'),
