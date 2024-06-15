ce7f64be9527ab1659e30e96dfb1bffd78afb01c
Rafael Matias <rafamatias102@gmail.com>
2024-06-15 17:52:26 +0000

    Improved 'New Folder' form pop-up in drive view. Implemented functionality for 'New Folder' button. Fixed styling issues. Implemented animations

---
+++ databox/user_drive/static/user_drive/js/base_drive.js
@@ -0,0 +1,43 @@
+window.onload = () => {
+
+console.log('this is JS for base drive');
+
+// CONSTANTS
+const newFolderButton = document.querySelector('#new-folder');
+const popUpView = document.querySelector('#pop-up-view');
+const popUpForm = document.querySelector('#create-new-folder-wrapper');
+const cancelButton = document.querySelector('#cancel');
+
+
+// 'NEW FOLDER' BUTTON 
+newFolderButton.addEventListener('click', () => {
+    console.log("button clicked");
+    popUpView.style.display = "flex";
+    popUpForm.style.display = "flex"
+    popUpView.setAttribute('active', 'True');
+    popUpForm.setAttribute('active', 'True');
+
+    popUpView.addEventListener('click', () => {
+        popUpView.style.display = "none";
+        popUpForm.style.display = "none"
+        popUpView.setAttribute('active', 'False');
+        popUpForm.setAttribute('active', 'False');
+    })
+});
+
+// 'UPLOAD FILES' BUTTON 
+
+
+
+// NEW FOLDER FORM 'CANCEL' BUTTON
+cancelButtonaddEventListener('click', () => {
+    console.log("cancel button clicked");
+    popUpView.style.display = "none";
+    popUpForm.style.display = "none"
+    popUpView.setAttribute('active', 'False');
+    popUpForm.setAttribute('active', 'False');
+});
+
+
+// NEW FOLDER FORM 'SUBMIT' BUTTON
+}
