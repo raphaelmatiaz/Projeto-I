7c653b47b321341b81dffee5187dee5dcd3d4f20
Rafael Matias <rafamatias102@gmail.com>
2024-06-19 19:02:07 +0000

    [FIX] Fixed poetry venv cache bug

--- databox/user_drive/forms.py
+++ databox/user_drive/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from .models import Folder  
-from django.db import models
+
 
 class NewFolderForm(forms.ModelForm):
     class Meta:
--- databox/user_drive/models.py
+++ databox/user_drive/models.py
@@ -1,35 +1,43 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-
-
 # Create your models here.
-class Drive(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    folders = models.ForeignKey('Folder', blank=True, null=True, related_name='DriveFolderChildren', default=None, on_delete=models.SET_DEFAULT)
-    files = models.ForeignKey('File', blank=True, null=True, related_name='DriveFileChildren', default=None, on_delete=models.SET_DEFAULT)
 
-    def __str__(self):
-            return self.user
 
 class Folder(models.Model):
-    name = models.CharField(max_length=50, unique=True)  # Ensure unique folder names
-    parent = models.ForeignKey('Folder', blank=True, null=True, related_name='FolderChildren', on_delete=models.CASCADE)
+    name = models.CharField(max_length=50, unique=True)  
+    parent = models.ForeignKey('Folder', blank=True, null=True, on_delete=models.CASCADE)
+    drive = models.ForeignKey('Drive', on_delete=models.CASCADE)
 
-    def get_default_drive(self):
-        # Logic to determine the default Drive for a new Folder based on self.user
-        return Drive.objects.get_or_create(user=self.user)[0]
-
-    drive_parent = models.ForeignKey('Drive', related_name='FolderDrive', on_delete=models.CASCADE, null=True, default=get_default_drive)
-
-    def __str__(self):
-        return self.name
-
+class Drive(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    # files = models.ForeignKey('File', blank=True, null=True, default=None, on_delete=models.SET_DEFAULT)
+    # folders = models.ForeignKey('Folder', blank=True, null=True, default=None, on_delete=models.SET_DEFAULT)
 
 class File(models.Model):
     name = models.CharField(max_length=50)
     file = models.FileField()
-    parent = models.ForeignKey('Folder', blank=True, null=True, related_name='FileChildren', on_delete=models.CASCADE)
+    drive = models.ForeignKey('Drive', on_delete=models.CASCADE)
 
-    def __str__(self):
-        return self.name
+
+
+# NOTA em modelos de db vamos do elemento mais pequeno ao maior. A ForeignKey deve ser posta no elemento 'inferior' àquele ao qual o queremos conectar:
+
+# Exemplo: uma escola tem váarios alunos    um aluno so pode estar em uma escola
+
+            # class Escola(models.Model):
+            #     aluno = models.OneToOneField('Aluno')
+
+
+            # class Aluno(models.Model):
+            #     escola = models.ForeignKey('Escola')
+
+
+# Exemplo: uma equipa de futbol tem varios jogadores            um jogador so pode pertencer a uma equipa
+
+
+            # class Equipa(models.Model):
+            #     jogador = models.OneToOneField('Jogador')
+    
+            # class Jogador(models.Model):
+            #     equipa = models.ForeignKey('Equipa')
